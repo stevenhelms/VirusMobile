81526ccbba68aa93674cba0c32121b637394980f
Steve Helms <steve@cadenrock.com>
2020-03-13 09:41:28 -0500

    Adding demo data for lists.

---
+++ constants/demo-data.js
@@ -0,0 +1,15 @@
+export default {
+  virusCarriers: [{
+        id: 1,
+        name: 'Fury Road Albino'
+    },
+    {
+        id: 2,
+        name: 'MattMatthew',
+    },
+    {
+        id: 3,
+        name: 'Pointy-Haired Boss',
+    }
+  ]
+};
